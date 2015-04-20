4f61c2dd510674bb721a395a02f488a818968ab5
Ned Twigg <ned.twigg@diffplug.com>
2015-04-20 01:51:21 -0700

    It's very useful that ErrorHandler can wrap functions that return a value.  In order to wrap a fallible function, we need a default value to return in case the function fails.  This leads to method signatures like `getWithDefault()`.
    But for an ErrorHandler which always rethrows exceptions as RuntimeExceptions, such as ErrorHandler.rethrow(), the default value is guaranteed to never be used, and it's a cognitive waste to have to think one up.
    
    So, we now have two subclasses of ErrorHandler: Handling (which can throw a RuntimeException but doesn't have to) and Rethrowing (which always throws a RuntimeException by construction).  This lets each type have the correct methods when dealing with fallible functions that return values.

--- src/com/diffplug/common/base/ErrorHandler.java
+++ src/com/diffplug/common/base/ErrorHandler.java
@@ -27,12 +27,29 @@ import java.util.function.Supplier;
  * functional interface, ErrorHandler is useful as a concise way to
  * specify how errors will be handled. 
  */
-public class ErrorHandler {
-	private final Consumer<Throwable> handler;
+public abstract class ErrorHandler {
+	protected final Consumer<Throwable> handler;
 
-	/** Creates an OnError which will pass any errors to this handler. */
-	public static ErrorHandler onError(Consumer<Throwable> handler) {
-		return new ErrorHandler(handler);
+	/**
+	 * Creates an ErrorHandler.Rethrowing which transforms any exceptions it receives into a RuntimeException
+	 * as specified by the given function, and then throws that RuntimeException.
+	 * 
+	 * If that function happens to throw a RuntimeException itself, that'll work just fine, but it's ugly.
+	 */
+	public static Rethrowing createRethrowAs(Function<Throwable, RuntimeException> transform) {
+		return new Rethrowing(transform);
+	}
+
+	/**
+	 * Creates an ErrorHandler.Handling which passes any exceptions it receives
+	 * to the given handler.
+	 * 
+	 * The handler is free to throw a RuntimeException if it wants to. If it always
+	 * throws a RuntimeException, then you should instead create an ErrorHandler.Rethrowing
+	 * using creeateRethrowAs().
+	 */
+	public static Handling createHandleWith(Consumer<Throwable> handler) {
+		return new Handling(handler);
 	}
 
 	protected ErrorHandler(Consumer<Throwable> error) {
@@ -40,20 +57,18 @@ public class ErrorHandler {
 	}
 
 	/** Suppresses errors entirely. */
-	public static ErrorHandler suppress() {
+	public static Handling suppress() {
 		return suppress;
 	}
 
-	private static final ErrorHandler suppress = new ErrorHandler(obj -> {});
+	private static final Handling suppress = createHandleWith(obj -> {});
 
 	/** Rethrows any exceptions as runtime exceptions. */
-	public static ErrorHandler rethrow() {
+	public static Rethrowing rethrow() {
 		return rethrow;
 	}
 
-	private static final ErrorHandler rethrow = new ErrorHandler(error -> {
-		throw asRuntime(error);
-	});
+	private static final Rethrowing rethrow = createRethrowAs(ErrorHandler::asRuntime);
 
 	/**
 	 * Logs any exceptions.
@@ -61,7 +76,7 @@ public class ErrorHandler {
 	 * By default, log() just calls Exception.printStackTrace(). To modify this behavior
 	 * in your application, call DurianPlugins.registerErrorHandlerLog() on startup.
 	 */
-	public static ErrorHandler log() {
+	public static Handling log() {
 		if (log == null) {
 			// There is an acceptable race condition here - log might get set multiple times.
 			// This would happen if multiple threads called log() at the same time
@@ -74,12 +89,12 @@ public class ErrorHandler {
 			// It is important for this method to be fast, so it's better to accept
 			// that log() might return different ErrorHandler instances which are wrapping
 			// the same actual Consumer<Throwable>.
-			log = new ErrorHandler(DurianPlugins.getInstance().getErrorHandlerLog());
+			log = createHandleWith(DurianPlugins.getInstance().getErrorHandlerLog());
 		}
 		return log;
 	}
 
-	private static ErrorHandler log;
+	private static Handling log;
 
 	/**
 	 * Opens a dialog to notify the user of any exceptions.
@@ -87,15 +102,15 @@ public class ErrorHandler {
 	 * By default, log() just calls Exception.printStackTrace(). To modify this behavior
 	 * in your application, call DurianPlugins.registerErrorHandlerDialog() on startup.
 	 */
-	public static ErrorHandler dialog() {
+	public static Handling dialog() {
 		if (dialog == null) {
 			// There is an acceptable race condition here.  See ErrorHandler.log() for details.
-			dialog = new ErrorHandler(DurianPlugins.getInstance().getErrorHandlerDialog());
+			dialog = createHandleWith(DurianPlugins.getInstance().getErrorHandlerDialog());
 		}
 		return dialog;
 	}
 
-	private static ErrorHandler dialog;
+	private static Handling dialog;
 
 	/** Passes the given error to be handled by the ErrorHandler. */
 	public final void handle(Throwable error) {
@@ -105,23 +120,6 @@ public class ErrorHandler {
 	/** Attempts to run the given runnable. */
 	public final void run(Throwing.Runnable runnable) {
 		wrap(runnable).run();
-	}
-
-	/** Attempts to call the given supplier, returns onFailure if there is a failure. */
-	public final <T> T getWithDefault(Throwing.Supplier<T> supplier, T onFailure) {
-		return wrapWithDefault(supplier, onFailure).get();
-	}
-
-	/**
-	 * tryGetWithDefault() doesn't make sense for ErrorHandler.rethrow(), because the default
-	 * value will never be returned. This we provide this one-off method for 
-	 */
-	public static <T> T rethrowGet(Throwing.Supplier<T> supplier) {
-		try {
-			return supplier.get();
-		} catch (Throwable e) {
-			throw asRuntime(e);
-		}
 	}
 
 	/** Returns a Runnable whose exceptions are handled by this ErrorHandler. */
@@ -146,28 +144,88 @@ public class ErrorHandler {
 		};
 	}
 
-	/** Attempts to call the given function, and returns the given value on failure. */
-	public final <T, R> Function<T, R> wrapWithDefault(Throwing.Function<T, R> function, R onFailure) {
-		return input -> {
-			try {
-				return function.apply(input);
-			} catch (Throwable e) {
-				handler.accept(e);
-				return onFailure;
-			}
-		};
+	/**
+	 * An ErrorHandler which is free to rethrow the exception, but it might not.
+	 * 
+	 * If we want to wrap a method with a return value, since the handler might
+	 * not throw an exception, we need a default value to return.
+	 */
+	public static class Handling extends ErrorHandler {
+		protected Handling(Consumer<Throwable> error) {
+			super(error);
+		}
+
+		/** Attempts to call the given supplier, returns onFailure if there is a failure. */
+		public final <T> T getWithDefault(Throwing.Supplier<T> supplier, T onFailure) {
+			return wrapWithDefault(supplier, onFailure).get();
+		}
+
+		/** Attempts to call the given supplier, and returns the given value on failure. */
+		public final <T> Supplier<T> wrapWithDefault(Throwing.Supplier<T> supplier, T onFailure) {
+			return () -> {
+				try {
+					return supplier.get();
+				} catch (Throwable e) {
+					handler.accept(e);
+					return onFailure;
+				}
+			};
+		}
+
+		/** Attempts to call the given function, and returns the given value on failure. */
+		public final <T, R> Function<T, R> wrapWithDefault(Throwing.Function<T, R> function, R onFailure) {
+			return input -> {
+				try {
+					return function.apply(input);
+				} catch (Throwable e) {
+					handler.accept(e);
+					return onFailure;
+				}
+			};
+		}
 	}
 
-	/** Attempts to call the given supplier, and returns the given value on failure. */
-	public final <T> Supplier<T> wrapWithDefault(Throwing.Supplier<T> supplier, T onFailure) {
-		return () -> {
-			try {
-				return supplier.get();
-			} catch (Throwable e) {
-				handler.accept(e);
-				return onFailure;
-			}
-		};
+	/**
+	 * An ErrorHandler which is guaranteed to always throw a RuntimeException.
+	 * 
+	 * If we want to wrap a method with a return value, it's pointless to specify
+	 * a default value because if the wrapped method fails, a RuntimeException is
+	 * guaranteed to throw.
+	 */
+	public static class Rethrowing extends ErrorHandler {
+		private final Function<Throwable, RuntimeException> transform;
+
+		protected Rethrowing(Function<Throwable, RuntimeException> transform) {
+			super(error -> { throw transform.apply(error); });
+			this.transform = transform;
+		}
+
+		/** Attempts to call the given supplier, throws some kind of RuntimeException on failure. */
+		public final <T> T get(Throwing.Supplier<T> supplier) {
+			return wrap(supplier).get();
+		}
+
+		/** Attempts to call the given supplier, throws some kind of RuntimeException on failure. */
+		public final <T> Supplier<T> wrap(Throwing.Supplier<T> supplier) {
+			return () -> {
+				try {
+					return supplier.get();
+				} catch (Throwable e) {
+					throw transform.apply(e);
+				}
+			};
+		}
+
+		/** Attempts to call the given function, throws some kind of RuntimeException on failure. */
+		public final <T, R> Function<T, R> wrap(Throwing.Function<T, R> function) {
+			return arg -> {
+				try {
+					return function.apply(arg);
+				} catch (Throwable e) {
+					throw transform.apply(e);
+				}
+			};
+		}
 	}
 
 	/** Converts the given exception to a RuntimeException, with a minimum of new exceptions to obscure the cause. */
