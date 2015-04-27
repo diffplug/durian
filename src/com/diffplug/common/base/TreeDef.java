0328a5f417c9b61b735b6192ff4791c5535869cd
Ned Twigg <ned.twigg@diffplug.com>
2015-04-26 17:43:36 -0700

    Added a fluent mechanism for filtering TreeDefs.

--- src/com/diffplug/common/base/TreeDef.java
+++ src/com/diffplug/common/base/TreeDef.java
@@ -17,11 +17,19 @@ package com.diffplug.common.base;
 
 import java.util.List;
 import java.util.function.Function;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
 
 /** Function which defines a Tree structure based on a root object. */
+@FunctionalInterface
 public interface TreeDef<T> {
 	/** Returns all the children of the given node. */
 	List<T> childrenOf(T node);
+
+	/** Returns a new TreeDef which whose childrenOf() method is filtered by the given predicate. */
+	default TreeDef<T> filter(Predicate<T> predicate) {
+		return TreeDef.of(node -> filteredList(childrenOf(node), predicate));
+	}
 
 	/** Creates a TreeDef which is implemented by the given function. */
 	public static <T> TreeDef<T> of(Function<T, List<T>> childFunc) {
@@ -43,8 +51,20 @@ public interface TreeDef<T> {
 	 * this TreeDef.Parented are likely to fail in unexpected ways.
 	 */
 	public interface Parented<T> extends TreeDef<T> {
-		/** Returns all the parent of the given node. */
+		/** Returns the parent of the given node. */
 		T parentOf(T node);
+
+		/** Returns a new TreeDef which whose childrenOf() and parentOf() methods are filtered by the given predicate. */
+		@Override
+		default Parented<T> filter(Predicate<T> predicate) {
+			return of(node -> filteredList(childrenOf(node), predicate), node -> {
+				if (predicate.test(node)) {
+					return parentOf(node);
+				} else {
+					return null;
+				}
+			});
+		}
 
 		/** Creates a TreeDef.Parented which is implemented by the given two functions. */
 		public static <T> TreeDef.Parented<T> of(Function<T, List<T>> childFunc, Function<T, T> parentFunc) {
@@ -60,5 +80,10 @@ public interface TreeDef<T> {
 				}
 			};
 		}
+	}
+
+	/** Returns a filtered version of the given list. */
+	static <T> List<T> filteredList(List<T> unfiltered, Predicate<T> filter) {
+		return unfiltered.stream().filter(filter).collect(Collectors.toList());
 	}
 }
