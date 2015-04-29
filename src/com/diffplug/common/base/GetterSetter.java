bb86757f7227e995cfc21e2c5531b7818235716e
Ned Twigg <ned.twigg@diffplug.com>
2015-04-28 18:11:56 -0700

    Added GetterSetter to durian.

---
+++ src/com/diffplug/common/base/GetterSetter.java
@@ -0,0 +1,54 @@
+/**
+ * Copyright 2015 DiffPlug
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.diffplug.common.base;
+
+import java.util.function.BiConsumer;
+import java.util.function.Consumer;
+import java.util.function.Function;
+import java.util.function.Supplier;
+
+/** Provides get/set access to some field. */
+public interface GetterSetter<T> extends Supplier<T>, Consumer<T> {
+	/** Creates a GetterSetter from a Supplier and a Consumer. */
+	public static <T> GetterSetter<T> from(Supplier<T> getter, Consumer<T> setter) {
+		return new GetterSetter<T>() {
+			@Override
+			public T get() {
+				return getter.get();
+			}
+
+			@Override
+			public void accept(T value) {
+				setter.accept(value);
+			}
+		};
+	}
+
+	/** Creates a GetterSetter from an argument and two functions which operate on that argument. */
+	public static <T, V> GetterSetter<T> from(V target, Function<V, T> getter, BiConsumer<V, T> setter) {
+		return new GetterSetter<T>() {
+			@Override
+			public T get() {
+				return getter.apply(target);
+			}
+
+			@Override
+			public void accept(T value) {
+				setter.accept(target, value);
+			}
+		};
+	}
+}
---
+++ test/com/diffplug/common/base/GetterSetterTest.java
@@ -0,0 +1,60 @@
+/**
+ * Copyright 2015 DiffPlug
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.diffplug.common.base;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+/** Stupid-simple test for the very simple GetterSetter class. */
+public class GetterSetterTest {
+	private static String staticValue;
+
+	private static String getStaticValue() {
+		return staticValue;
+	}
+
+	private static void setStaticValue(String value) {
+		staticValue = value;
+	}
+
+	@Test
+	public void testStatic() {
+		GetterSetter<String> forValue = GetterSetter.from(GetterSetterTest::getStaticValue, GetterSetterTest::setStaticValue);
+		forValue.accept("A");
+		Assert.assertEquals("A", forValue.get());
+		forValue.accept("B");
+		Assert.assertEquals("B", forValue.get());
+	}
+
+	private String value;
+
+	private String getValue() {
+		return value;
+	}
+
+	private void setValue(String value) {
+		this.value = value;
+	}
+
+	@Test
+	public void testInstance() {
+		GetterSetter<String> forValue = GetterSetter.from(this, GetterSetterTest::getValue, GetterSetterTest::setValue);
+		forValue.accept("A");
+		Assert.assertEquals("A", forValue.get());
+		forValue.accept("B");
+		Assert.assertEquals("B", forValue.get());
+	}
+}
