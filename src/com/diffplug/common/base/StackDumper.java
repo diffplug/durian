d58f8d032c1ad03cb8f3f4310cabbb503501a833
Ned Twigg <ned.twigg@diffplug.com>
2015-05-07 16:40:31 -0400

    Added the StackDumper, for debugging stuff.

---
+++ src/com/diffplug/common/base/StackDumper.java
@@ -0,0 +1,141 @@
+/**
+ * Copyright 2015 DiffPlug
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.diffplug.common.base;
+
+import java.io.PrintStream;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.ListIterator;
+import java.util.function.Predicate;
+
+/** Utility methods for dumping the stack - arbitrarily or at specific trigger points (such as when a certain string prints to console). */
+public class StackDumper {
+	static StringPrinter pristineSysErr = new StringPrinter(System.err::print);
+
+	/** Dumps the given message and stack to the system error console. */
+	public static void dump(String message, List<StackTraceElement> stack) {
+		printEmphasized(message + "\n" + stackTraceToString(stack));
+	}
+
+	/** Dumps the given message and stack to the system error console. */
+	public static void dump(String message, StackTraceElement[] stackTrace) {
+		dump(message, Arrays.asList(stackTrace));
+	}
+
+	/** Dumps the given message and exception stack to the system error console */
+	public static void dump(String message, Throwable exception) {
+		printEmphasized(StringPrinter.buildString(printer -> {
+			printer.println(message);
+			exception.printStackTrace(printer.toPrintWriter());
+		}));
+	}
+
+	/** Dumps the given stack to the system error console. */
+	public static void dump(String message) {
+		dump(message, captureStackBelow(StackDumper.class));
+	}
+
+	/** Dumps a stack trace anytime the trigger string is printed to System.out. */
+	public static void dumpWhenSysOutContains(String trigger) {
+		System.setOut(wrapAndDumpWhenContains(System.out, trigger));
+	}
+
+	/** Dumps a stack trace anytime trigger string is printed to System.err. */
+	public static void dumpWhenSysErrContains(String trigger) {
+		System.setErr(wrapAndDumpWhenContains(System.err, trigger));
+	}
+
+	/**
+	 * Returns a PrintStream which will redirect all of its output to the source PrintStream. If
+	 * the trigger string is passed through the wrapped PrintStream, then it will dump the
+	 * stack trace of the call that printed the trigger.
+	 * 
+	 * @param source
+	 *            the returned PrintStream will delegate to this stream
+	 * @param trigger
+	 *            the string which triggers a stack dump
+	 * @return a PrintStream with the above properties
+	 */
+	public static PrintStream wrapAndDumpWhenContains(final PrintStream source, final String trigger) {
+		StringPrinter wrapped = new StringPrinter(StringPrinter.stringsToLines(perLine -> {
+			source.println(perLine);
+			if (perLine.contains(trigger)) {
+				dump("Triggered by " + trigger);
+			}
+		}));
+		return wrapped.toPrintStream();
+	}
+
+	/** Converts a list of stack trace elements to a String similar to Throwable.printStackTrace(). */
+	private static String stackTraceToString(List<StackTraceElement> stack) {
+		return StringPrinter.buildString(printer -> {
+			for (StackTraceElement element : stack) {
+				printer.print("at ");
+				printer.print(element.getClassName());
+				printer.print(".");
+				printer.print(element.getMethodName());
+				printer.print("(");
+				printer.print(element.getFileName());
+				printer.print(":");
+				printer.print(Integer.toString(element.getLineNumber()));
+				printer.println(")");
+			}
+		});
+	}
+
+	/** Captures all of the current stack which is below the given classes. */
+	public static List<StackTraceElement> captureStackBelow(Class<?>... clazzes) {
+		List<Class<?>> toIgnore = new ArrayList<>(clazzes.length + 1);
+		toIgnore.addAll(Arrays.asList(clazzes));
+		toIgnore.add(StackDumper.class);
+
+		Predicate<StackTraceElement> isSkipped = element -> toIgnore.stream().anyMatch(clazz -> {
+			String name = element.getClassName();
+			return name.equals(clazz.getName()) || name.startsWith(clazz.getName() + "$$Lambda");
+		});
+
+		List<StackTraceElement> rawStack = Arrays.asList(Thread.currentThread().getStackTrace());
+		ListIterator<StackTraceElement> iterator = rawStack.listIterator();
+
+		// iterate until we find something skipped
+		while (iterator.hasNext() && !isSkipped.test(iterator.next())) {}
+
+		boolean foundSomethingToSkip = iterator.hasNext();
+		if (foundSomethingToSkip) {
+			// iterate unti we find something not skipped
+			while (iterator.hasNext() && isSkipped.test(iterator.next())) {}
+			// the filtering was successful!
+			return rawStack.subList(iterator.previousIndex(), rawStack.size());
+		} else {
+			// we didn't find something to skip, so we'll return the whole stack
+			return rawStack;
+		}
+	}
+
+	/** 
+	 * Prints the given string to the the given printer, wrapped in hierarchy-friendly
+	 * braces.  Useful for emphasizing a specific event from a sea of logging statements.
+	 */
+	private static void printEmphasized(String toPrint) {
+		// print the triggered header
+		pristineSysErr.println("+----------\\");
+		for (String line : toPrint.split("\n")) {
+			pristineSysErr.println("| " + line);
+		}
+		pristineSysErr.println("+----------/");
+	}
+}
---
+++ test/com/diffplug/common/base/StackDumperTest.java
@@ -0,0 +1,166 @@
+/**
+ * Copyright 2015 DiffPlug
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.diffplug.common.base;
+
+import java.io.PrintStream;
+
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import com.diffplug.common.base.Box;
+import com.diffplug.common.base.StackDumper;
+import com.diffplug.common.base.StringPrinter;
+import com.diffplug.common.base.Throwing;
+
+public class StackDumperTest {
+	PrintStream cleanOut, cleanErr;
+
+	@Before
+	public void setupStreams() {
+		testCaseOut.setLength(0);
+		testCaseErr.setLength(0);
+
+		cleanOut = System.out;
+		cleanErr = System.err;
+
+		System.setOut(new StringPrinter(str -> {
+			testCaseOut.append(str.replace("\r", ""));
+		}).toPrintStream());
+		System.setErr(new StringPrinter(str -> {
+			testCaseErr.append(str.replace("\r", ""));
+		}).toPrintStream());
+		StackDumper.pristineSysErr = new StringPrinter(System.err::print);
+	}
+
+	@After
+	public void restoreStreams() {
+		System.setOut(cleanOut);
+		System.setErr(cleanErr);
+	}
+
+	/** Everything which has been printed to System.out during this test case. */
+	StringBuilder testCaseOut = new StringBuilder();
+	/** Everything which has been printed to System.err during this test case. */
+	StringBuilder testCaseErr = new StringBuilder();
+
+	private void runWithCleanStack(Throwing.Runnable runnable) throws Throwable {
+		Box<Throwable> testError = Box.empty();
+		Thread thread = new Thread(() -> {
+			try {
+				runnable.run();
+			} catch (Throwable e) {
+				testError.set(e);
+			}
+		});
+		thread.start();
+		thread.join();
+		if (testError.get() != null) {
+			throw testError.get();
+		}
+	}
+
+	/** Make sure that the test harness actually works. */
+	@Test(expected = IllegalArgumentException.class)
+	public void testTestHarness() throws Throwable {
+		System.out.println("System.out");
+		System.err.println("System.err");
+
+		Assert.assertEquals("System.out\n", testCaseOut.toString());
+		Assert.assertEquals("System.err\n", testCaseErr.toString());
+
+		runWithCleanStack(() -> {
+			throw new IllegalArgumentException("");
+		});
+	}
+
+	@Test
+	public void testDump() throws Throwable {
+		// dump with no stack
+		runWithCleanStack(() -> {
+			StackDumper.dump("some message");
+			Assert.assertEquals(StringPrinter.buildStringFromLines(
+					"+----------\\",
+					"| some message",
+					"| at com.diffplug.common.base.StackDumperTest.lambda$4(StackDumperTest.java:95)",
+					"| at com.diffplug.common.base.StackDumperTest$$Lambda$10/914424520.run(null:-1)",
+					"| at com.diffplug.common.base.StackDumperTest.lambda$2(StackDumperTest.java:65)",
+					"| at com.diffplug.common.base.StackDumperTest$$Lambda$11/2143192188.run(null:-1)",
+					"| at java.lang.Thread.run(null:-1)",
+					"+----------/"), testCaseErr.toString());
+		});
+	}
+
+	@Test
+	public void testDumpFiltered() throws Throwable {
+		// dump with no stack
+		runWithCleanStack(() -> {
+			StackDumper.dump("some message", StackDumper.captureStackBelow(StackDumperTest.class));
+			Assert.assertEquals(StringPrinter.buildStringFromLines(
+					"+----------\\",
+					"| some message",
+					"| at java.lang.Thread.run(null:-1)",
+					"+----------/"), testCaseErr.toString());
+		});
+	}
+
+	@Test
+	public void testDumpWhenSysOut() throws Throwable {
+		// dump with no stack
+		runWithCleanStack(() -> {
+			StackDumper.dumpWhenSysOutContains("Who did this?");
+
+			System.out.println("What!?");
+			Assert.assertEquals("What!?\n", testCaseOut.toString());
+			Assert.assertEquals("", testCaseErr.toString());
+
+			System.out.println("Who did this?");
+			Assert.assertEquals("What!?\nWho did this?\n", testCaseOut.toString());
+			String errOutput = testCaseErr.toString();
+			Assert.assertTrue(errOutput.startsWith(StringPrinter.buildStringFromLines(
+					"+----------\\",
+					"| Triggered by Who did this?")));
+			Assert.assertTrue(errOutput.endsWith(StringPrinter.buildStringFromLines(
+					"| at java.lang.Thread.run(null:-1)",
+					"+----------/")));
+		});
+	}
+
+	@Test
+	public void testDumpWhenSysErr() throws Throwable {
+		// dump with no stack
+		runWithCleanStack(() -> {
+			StackDumper.dumpWhenSysErrContains("Who did this?");
+
+			System.err.println("What!?");
+			Assert.assertEquals("", testCaseOut.toString());
+			Assert.assertEquals("What!?\n", testCaseErr.toString());
+
+			System.err.println("Who did this?");
+			Assert.assertEquals("", testCaseOut.toString());
+			String errOutput = testCaseErr.toString();
+			Assert.assertTrue(errOutput.startsWith(StringPrinter.buildStringFromLines(
+					"What!?",
+					"Who did this?",
+					"+----------\\",
+					"| Triggered by Who did this?")));
+			Assert.assertTrue(errOutput.endsWith(StringPrinter.buildStringFromLines(
+					"| at java.lang.Thread.run(null:-1)",
+					"+----------/")));
+		});
+	}
+}
