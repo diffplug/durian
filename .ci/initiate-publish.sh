314ef93a20a8036b60d5a2cb72da79237b2f314f
travis-ci <travis@travis-ci.org>
2015-05-27 02:50:05 -0700

    Travis can now publish artifacts and javadoc automatically.

--- .ci/initiate-publish.sh
+++ .ci/initiate-publish.sh
@@ -2,13 +2,13 @@
 # This script initiates the Gradle publishing task when pushes to master occur.
 # NOTE: Travis-CI can only publish SNAPSHOT versions.
 if [ "$TRAVIS_REPO_SLUG" == "DiffPlug/durian" ] && [ "$TRAVIS_PULL_REQUEST" == "false" ] && [ "$TRAVIS_BRANCH" == "master" ]; then
-	if [[ $(./gradlew -q getVersion) != *SNAPSHOT* ]]; then
-			echo 'Travis can only publish snapshots.'
-			return 0
+	if [[ $(./gradlew -q printVersion) != *SNAPSHOT* ]]; then
+		echo -e "Publishing snapshot\n"
+	else
+		echo -e "Publishing release\n"
 	fi
 
-	echo -e "Starting publish to Sonatype...\n"
-	./gradlew publish -Pnexus_user="${nexus_user}" -Pnexus_pass="${nexus_pass}"
+	./gradlew publish
 	RETVAL=$?
 
 	if [ $RETVAL -eq 0 ]; then
--- .ci/push-javadoc-to-gh-pages.sh
+++ .ci/push-javadoc-to-gh-pages.sh
@@ -1,23 +1,36 @@
 #!/bin/bash
-# This script initiates the Gradle publishing task when pushes to master occur.
+# This script initiates publishing the proper Javadoc page whenever pushes to master occur.
 # NOTE: Travis-CI can only publish SNAPSHOT versions.
 if [ "$TRAVIS_REPO_SLUG" == "DiffPlug/durian" ] && [ "$TRAVIS_PULL_REQUEST" == "false" ] && [ "$TRAVIS_BRANCH" == "master" ]; then
 
 	echo -e "Publishing javadoc...\n"
 
-	cp -R build/docs/javadoc $HOME/javadoc-latest
+	# copy javadoc from the build to ~/javadoc-temp
+	cp -R build/docs/javadoc $HOME/javadoc-temp
 
+	# clone a new copy durian to gh-pages
 	cd $HOME
 	git config --global user.email "travis@travis-ci.org"
 	git config --global user.name "travis-ci"
-	git clone --quiet --branch=gh-pages https://${gh_token}@github.com/ReadyTalk/swt-bling gh-pages > /dev/null
+	rm -rf $HOME/gh-pages
+	git clone --quiet --branch=gh-pages https://${gh_token}@github.com/DiffPlug/durian gh-pages > /dev/null
 
+	# copy the javadoc into the build
 	cd gh-pages
-	git rm -rf ./javadoc
-	cp -Rf $HOME/javadoc-latest ./javadoc
-	git add -f .
+	VERSION=$(./gradlew -q printVersion)
+	if [[ "$VERSION" != *SNAPSHOT* ]]; then
+		git rm -rf javadoc/${VERSION}
+		cp -Rf $HOME/javadoc-temp/ ./javadoc/${VERSION}/
+	else
+		git rm -rf javadoc/latest
+		cp -Rf $HOME/javadoc-temp/ ./javadoc/latest/
+	fi
+
+	# add all of the stuff and commit it
+	git add -f -A
 	git commit -m "Lastest javadoc on successful travis build $TRAVIS_BUILD_NUMBER auto-pushed to gh-pages"
 	git push -fq origin gh-pages > /dev/null
 
+	# success
 	echo -e "Published Javadoc to gh-pages.\n"
 fi
