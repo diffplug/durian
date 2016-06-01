86d123f0fcf85eba2d3f2ec0942eede43e75500b
Ned Twigg <ned.twigg@diffplug.com>
2016-06-01 13:59:28 -0700

    addfeature: StringPrinter, for piping strings around without turning them into bytes.

---
+++ core/sources/sources/com/diffplug/common/base/StringPrinter.java
@@ -0,0 +1,310 @@
+/*
+ * Original Guava code is copyright (C) 2015 The Guava Authors.
+ * Modifications from Guava are copyright (C) 2016 DiffPlug.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.diffplug.common.base;
+
+import static java.util.Objects.requireNonNull;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.PrintStream;
+import java.io.PrintWriter;
+import java.io.Writer;
+import java.nio.ByteBuffer;
+import java.nio.CharBuffer;
+import java.nio.charset.Charset;
+import java.nio.charset.CharsetDecoder;
+import java.nio.charset.CoderResult;
+import java.nio.charset.CodingErrorAction;
+import java.nio.charset.StandardCharsets;
+import java.util.function.Consumer;
+
+/**
+ * Pipes strings to a {@code Consumer<String>} through an API which unifies
+ * {@link OutputStream}, {@link PrintStream}, {@link Writer}, and {@link PrintWriter}.
+ *
+ * See {@link #buildString(Consumer)} for some common use cases.
+ */
+public final class StringPrinter {
+	private final Consumer<String> consumer;
+
+	/** StringPrinter will pass all the strings it receives to the given consumer. */
+	public StringPrinter(Consumer<String> consumer) {
+		this.consumer = requireNonNull(consumer);
+	}
+
+	/** Prints the string and a newline (always '\n'). */
+	public void println(String line) {
+		requireNonNull(line);
+		consumer.accept(line);
+		consumer.accept("\n");
+	}
+
+	/** Prints the string. */
+	public void print(String content) {
+		requireNonNull(content);
+		consumer.accept(content);
+	}
+
+	/**
+	 * Easy way to create a String using a StringPrinter.
+	 *
+	 * ```java
+	 * String currentStackTrace = StringPrinter.buildString(printer -> {
+	 *     new Throwable().printStackTrace(printer.toPrintStream());
+	 * });
+	 * ```
+	 */
+	public static String buildString(Consumer<StringPrinter> printer) {
+		StringBuilder builder = new StringBuilder();
+		printer.accept(new StringPrinter(builder::append));
+		return builder.toString();
+	}
+
+	/**
+	 * Utility method for creating multi-line strings
+	 * (joined with `\n`, never `\r\n`).
+	 */
+	public static String buildStringFromLines(String... lines) {
+		int numChars = lines.length;
+		for (String line : lines) {
+			numChars += line.length();
+		}
+		StringBuilder builder = new StringBuilder(numChars);
+		for (String line : lines) {
+			builder.append(line);
+			builder.append('\n');
+		}
+		return builder.toString();
+	}
+
+	/**
+	 * Calls {@link #toOutputStream(Charset)} with `charset=UTF-8`.
+	 */
+	public OutputStream toOutputStream() {
+		return toOutputStream(StandardCharsets.UTF_8);
+	}
+
+	/**
+	 * Creates an `OutputStream` which will print its content to this `StringPrinter`, encoding bytes according to the given `Charset`.
+	 * Doesn't matter if you close the stream or not, because StringPrinter doesn't have a close().
+	 *
+	 * Strings are sent to the consumer as soon as their constituent bytes are written to this OutputStream.
+	 *
+	 * The implementation is lifted from Apache commons-io.  Many thanks to them!
+	 */
+	public OutputStream toOutputStream(Charset charset) {
+		CharsetDecoder decoder = charset.newDecoder()
+				.onMalformedInput(CodingErrorAction.REPLACE)
+				.onUnmappableCharacter(CodingErrorAction.REPLACE)
+				.replaceWith("?");
+
+		ByteBuffer decoderIn = ByteBuffer.allocate(DECODER_BUFFER);
+		CharBuffer decoderOut = CharBuffer.allocate(DECODER_BUFFER);
+		return new OutputStream() {
+			@Override
+			public void write(final int b) throws IOException {
+				write(new byte[]{(byte) b});
+			}
+
+			@Override
+			public void write(byte[] b) throws IOException {
+				write(b, 0, b.length);
+			}
+
+			@Override
+			public void write(byte[] b, int off, int len) throws IOException {
+				while (len > 0) {
+					final int c = Math.min(len, decoderIn.remaining());
+					decoderIn.put(b, off, c);
+					processInput(false);
+					len -= c;
+					off += c;
+				}
+				flushOutput();
+			}
+
+			private void processInput(final boolean endOfInput) throws IOException {
+				// Prepare decoderIn for reading
+				decoderIn.flip();
+				CoderResult coderResult;
+				while (true) {
+					coderResult = decoder.decode(decoderIn, decoderOut, endOfInput);
+					if (coderResult.isOverflow()) {
+						flushOutput();
+					} else if (coderResult.isUnderflow()) {
+						break;
+					} else {
+						// The decoder is configured to replace malformed input and unmappable characters,
+						// so we should not get here.
+						throw new IOException("Unexpected coder result");
+					}
+				}
+				// Discard the bytes that have been read
+				decoderIn.compact();
+			}
+
+			private void flushOutput() throws IOException {
+				if (decoderOut.position() > 0) {
+					consumer.accept(new String(decoderOut.array(), 0, decoderOut.position()));
+					decoderOut.rewind();
+				}
+			}
+		};
+	}
+
+	/** Buffer size for decoding characters. */
+	private static final int DECODER_BUFFER = 128;
+
+	/**
+	 * Calls {@link #toPrintStream(Charset)} with `charset=UTF-8`.
+	 */
+	public PrintStream toPrintStream() {
+		return toPrintStream(StandardCharsets.UTF_8);
+	}
+
+	/**
+	 * Creates a `PrintStream` of the given charset, which passes its content to this `StringPrinter`.
+	 *
+	 * Note that `PrintStream.println` uses the system newline, while `StringPrinter.println` always uses `\n`.
+	 */
+	public PrintStream toPrintStream(Charset charset) {
+		return Errors.rethrow().get(() -> {
+			return new PrintStream(toOutputStream(charset), true, charset.name());
+		});
+	}
+
+	/** Creates a Writer which passes its content to this StringPrinter. */
+	public Writer toWriter() {
+		return new Writer() {
+			@Override
+			public Writer append(char c) {
+				consumer.accept(new String(new char[]{c}));
+				return this;
+			}
+
+			@Override
+			public Writer append(CharSequence csq) {
+				if (csq instanceof String) {
+					consumer.accept((String) csq);
+				} else {
+					consumer.accept(toStringSafely(csq));
+				}
+				return this;
+			}
+
+			@Override
+			public Writer append(CharSequence csq, int start, int end) {
+				if (csq instanceof String) {
+					consumer.accept(((String) csq).substring(start, end));
+				} else {
+					consumer.accept(toStringSafely(csq.subSequence(start, end)));
+				}
+				return this;
+			}
+
+			private String toStringSafely(CharSequence csq) {
+				String asString = csq.toString();
+				if (asString.length() == csq.length()) {
+					return asString;
+				} else {
+					// It's pretty easy to implement CharSequence.toString() incorrectly 
+					// http://stackoverflow.com/a/15870428/1153071
+					// but for String, we know we won't have them, thus the fast-path above
+					Errors.log().accept(new IllegalArgumentException(csq.getClass() + " did not implement toString() correctly."));
+					char[] chars = new char[csq.length()];
+					for (int i = 0; i < chars.length; ++i) {
+						chars[i] = csq.charAt(i);
+					}
+					return new String(chars);
+				}
+			}
+
+			@Override
+			public void close() throws IOException {}
+
+			@Override
+			public void flush() throws IOException {}
+
+			@Override
+			public void write(char[] cbuf, int off, int len) throws IOException {
+				consumer.accept(new String(cbuf, off, len));
+			}
+
+			@Override
+			public void write(String str) {
+				consumer.accept(str);
+			}
+
+			@Override
+			public void write(String str, int off, int len) {
+				consumer.accept(str.substring(off, off + len));
+			}
+		};
+	}
+
+	/**
+	 * Creates a PrintWriter which passes its content to this StringPrinter.
+	 *
+	 * Note that `PrintStream.println` uses the system newline, while `StringPrinter.println` always uses `\n`.
+	 */
+	public PrintWriter toPrintWriter() {
+		boolean autoflush = true;
+		return new PrintWriter(toWriter(), autoflush);
+	}
+
+	/**
+	 * Given a consumer of lines, creates a stateful consumer of strings
+	 * which will combine its input until it finds a newline, and
+	 * split its input when it contains multiple newlines.  Examples
+	 * make this more clear:
+	 * 
+	 * ```
+	 * "some", "\n", "simple ", "lines", "\n" -> "some", "simple lines"
+	 * "some\nsimple lines\n"                 -> "some", "simple lines"
+	 * "no newline\nno output"                -> "no newline"
+	 * ```
+	 * 
+	 * @param perLine a `Consumer<String>` which will receive strings which were terminated by newlines.
+	 *     (the newlines are stripped when they are passed to the consumer)
+	 * @return a `Consumer<String>` which accepts any strings, and will feed them to perLine. 
+	 */
+	public static Consumer<String> stringsToLines(Consumer<String> perLine) {
+		requireNonNull(perLine);
+		Box<String> leftover = Box.of("");
+		return rawString -> {
+			rawString = leftover.get() + rawString.replace("\r", "");
+
+			int lastIdx = 0;
+			int idx = 0;
+			while ((idx = rawString.indexOf('\n', lastIdx)) > -1) {
+				perLine.accept(rawString.substring(lastIdx, idx));
+				lastIdx = idx + 1;
+			}
+			leftover.set(rawString.substring(lastIdx));
+		};
+	}
+
+	/** Creates a `StringPrinter` directed at {@link System#out}. */
+	public static StringPrinter systemOut() {
+		return new StringPrinter(System.out::print);
+	}
+
+	/** Creates a `StringPrinter` directed at {@link System#err}. */
+	public static StringPrinter systemErr() {
+		return new StringPrinter(System.err::print);
+	}
+}
---
+++ core/tests/tests/com/diffplug/common/base/StringPrinterTest.java
@@ -0,0 +1,163 @@
+/*
+ * Original Guava code is copyright (C) 2015 The Guava Authors.
+ * Modifications from Guava are copyright (C) 2016 DiffPlug.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.diffplug.common.base;
+
+import java.io.OutputStream;
+import java.io.Writer;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+import java.util.Arrays;
+import java.util.function.Consumer;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class StringPrinterTest {
+	private static final String TEST_STR = "ABC123\nDo-Re-Mi\n";
+
+	private static void printTestStr(StringPrinter printer) {
+		printer.print("ABC");
+		printer.println("123");
+		printer.println("Do-Re-Mi");
+	}
+
+	@Test
+	public void testBasicFunctionality() {
+		StringBuilder builder = new StringBuilder();
+		StringPrinter printer = new StringPrinter(builder::append);
+		printTestStr(printer);
+		Assert.assertEquals(TEST_STR, builder.toString());
+	}
+
+	@Test
+	public void testStringCreator() {
+		Assert.assertEquals(TEST_STR, StringPrinter.buildString(printer -> {
+			printTestStr(printer);
+		}));
+	}
+
+	@Test
+	public void testToOutputStream() {
+		Assert.assertEquals(TEST_STR, StringPrinter.buildString(printer -> {
+			Errors.rethrow().run(() -> {
+				printer.toOutputStream(StandardCharsets.UTF_8).write(TEST_STR.getBytes(StandardCharsets.UTF_8));
+			});
+		}));
+	}
+
+	@Test
+	public void testToPrintStream() {
+		Assert.assertEquals(TEST_STR, StringPrinter.buildString(printer -> {
+			Errors.rethrow().run(() -> {
+				printer.toPrintStream().print(TEST_STR);
+			});
+		}));
+	}
+
+	/** Tests each method in turn. */
+	@Test
+	public void testToWriter() {
+		writerTestCase("c",
+				test -> test.append('c'));
+		writerTestCase(TEST_STR,
+				test -> test.append(TEST_STR));
+		writerTestCase(TEST_STR.substring(1, 5),
+				test -> test.append(TEST_STR, 1, 5));
+		writerTestCase(TEST_STR,
+				test -> test.write(TEST_STR.toCharArray()));
+		writerTestCase(TEST_STR.substring(1, 5),
+				test -> test.write(TEST_STR.toCharArray(), 1, 4));
+		writerTestCase("c",
+				test -> test.write('c'));
+	}
+
+	private void writerTestCase(String expected, Throwing.Consumer<Writer> underTest) {
+		Assert.assertEquals(expected, StringPrinter.buildString(printer -> {
+			Errors.rethrow().run(() -> {
+				underTest.accept(printer.toWriter());
+			});
+		}));
+	}
+
+	/** "I can eat glass" in Japanese, according to http://www.columbia.edu/~fdc/utf8/index.html#glass */
+	private static final String TEST_UTF = "私はガラスを食べられます。それは私を傷つけません。";
+
+	@Test
+	public void testEncodings() {
+		testCharset(StandardCharsets.UTF_8);
+		testCharset(StandardCharsets.UTF_16);
+		testCharset(StandardCharsets.UTF_16BE);
+		testCharset(StandardCharsets.UTF_16LE);
+	}
+
+	private void testCharset(Charset charset) {
+		// get the bytes for this string
+		byte[] bytes = TEST_UTF.getBytes(charset);
+
+		// write the complicated UTF string one byte at a time to an OutputStream
+		String byteByByteOutputStream = StringPrinter.buildString(printer -> {
+			Errors.rethrow().run(() -> {
+				OutputStream output = printer.toOutputStream(charset);
+				for (byte b : bytes) {
+					output.write(b);
+				}
+			});
+		});
+		Assert.assertEquals(TEST_UTF, byteByByteOutputStream);
+
+		// write the complicated UTF string one byte at a time to a PrintStream
+		String byteByBytePrintStream = StringPrinter.buildString(printer -> {
+			Errors.rethrow().run(() -> {
+				OutputStream output = printer.toPrintStream(charset);
+				for (byte b : bytes) {
+					output.write(b);
+				}
+			});
+		});
+		Assert.assertEquals(TEST_UTF, byteByBytePrintStream);
+	}
+
+	@Test
+	public void testStringsToLines() {
+		// requires assembly
+		testCaseStringsToLines("some\nsimple lines\n",
+				"some", "\n", "simple ", "lines", "\n");
+		// requires splitting
+		testCaseStringsToLines("some\nsimple lines\n",
+				"some\nsimple lines\n");
+		// ensure requires newline
+		testCaseStringsToLines("no newline\n",
+				"no newline\nno output");
+	}
+
+	private void testCaseStringsToLines(String expected, String... inputs) {
+		// assemble the result
+		StringBuilder result = new StringBuilder();
+		// create a harness for converting strings to lines
+		Consumer<String> underTest = StringPrinter.stringsToLines(perLine -> {
+			// there should be no newline
+			Assert.assertEquals(-1, perLine.indexOf('\n'));
+			// we'll append the result to StringBuilder
+			result.append(perLine);
+			result.append('\n');
+		});
+		// feed the input to the test harness
+		Arrays.asList(inputs).forEach(underTest::accept);
+
+		Assert.assertEquals(expected, result.toString());
+	}
+}
